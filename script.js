fe8bcf9ec81f5d265b3e82c6d4e9d3af2290ed64
kappyllo <33810546+kappyllo@users.noreply.github.com>
2022-05-13 12:18:52 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,64 @@
+'use strict';
+
+// document.querySelector('.message').textContent = 'Correct Number!';
+
+// document.querySelector('.number').textContent = 13;
+// document.querySelector('.score').textContent = 20;
+
+// document.querySelector('.guess').value = 10;
+
+let secretNumber = Math.trunc(Math.random() * 20) + 1;
+let score = 20;
+let highScore = 0;
+const displayMessage = function (message) {
+  document.querySelector('.message').textContent = message;
+};
+
+document.querySelector('.check').addEventListener('click', function () {
+  const guess = Number(document.querySelector('.guess').value);
+
+  if (!guess) {
+    displayMessage('No number!');
+  } else if (guess === secretNumber) {
+    displayMessage('You guesed! Congrats.');
+    document.querySelector('body').style.backgroundColor = '#60b347';
+    document.querySelector('.number').textContent = secretNumber;
+    document.querySelector('.number').style.width = '30rem';
+    if (score > highScore) {
+      highScore = score;
+      document.querySelector('.highscore').textContent = highScore;
+    }
+  } else if (guess !== secretNumber) {
+    if (score > 1) {
+      displayMessage(
+        guess < secretNumber
+          ? 'Try again, your number is too low.'
+          : 'Try again, your number is too high.'
+      );
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      displayMessage('U lost, noobie.');
+      score = '1';
+      document.querySelector('.score').textContent = 0;
+    }
+  }
+});
+
+document.querySelector('.again').addEventListener('click', function () {
+  score = 20;
+  document.querySelector('.score').textContent = score;
+  document.querySelector('.guess').value = '';
+  displayMessage('Start guessing...');
+  document.querySelector('body').style.backgroundColor = '#222';
+  document.querySelector('.number').style.width = '15rem';
+  secretNumber = Math.trunc(Math.random() * 20) + 1;
+  document.querySelector('.number').textContent = '?';
+});
+
+const numberbox = document.querySelector('.guess');
+numberbox.addEventListener('keypress', function onEvent(event) {
+  if (event.key === 'Enter') {
+    document.querySelector('.check').click();
+  }
+});
